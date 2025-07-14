293e1db4d2daa734feadf0786c8eb13e9d49983d
CinverEsp <cinver.espinoza.v@uni.pe>
2025-07-14 15:39:15 -0500

    app: Añadir pruebas ci usando un archivo bash

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -6,10 +6,7 @@ flake8 /src #[anotaciones]
 shellcheck scripts/ci.sh
 # pasar pruebas unitarias
 pytest --maxfail=1 --disable-warnings --cov=src
-
 # generar reporte html 
-
+pytest tests/ --cov=microservicio-A --cov=microservicio-B --cov=microservicio-C --cov-report=html --cov-report=term
 # guardar 5 ultimos commits en un CHANGELOG.md
-git log --pretty=format
-
-# levantar malla en minikube
+git log --oneline -n 5 >> CHANGELOG.md
